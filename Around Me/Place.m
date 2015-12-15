d42c674370e8b5e30029816645018ea541c15448
Jason R Alexander <sunnysidesounds@gmail.com>
2015-12-14 21:50:12 -0800

    Initial commit of project

---
+++ Around Me/Place.m
@@ -0,0 +1,35 @@
+//
+//  Place.m
+//  Around Me
+//
+//  Created by jdistler on 11.02.13.
+//  Copyright (c) 2013 Jean-Pierre Distler. All rights reserved.
+//
+
+#import "Place.h"
+
+@implementation Place
+
+- (id)initWithLocation:(CLLocation *)location reference:(NSString *)reference name:(NSString *)name address:(NSString *)address {
+	NSLog(@"Place.initWithLocation");
+    if((self = [super init])) {
+		_location = location;
+		_reference = [reference copy];
+		_placeName = [name copy];
+		_address = [address copy];
+	}
+	
+	return self;
+}
+
+- (NSString *)infoText {
+	NSLog(@"Place.infoText");
+    return [NSString stringWithFormat:@"Name:%@\nAddress:%@\nPhone:%@\nWeb:%@", _placeName, _address, _phoneNumber, _website];
+}
+
+- (NSString *)description {
+	NSLog(@"Place.description");
+    return [NSString stringWithFormat:@"%@ Name:%@, location:%@", [super description], _placeName, _location];
+}
+
+@end
